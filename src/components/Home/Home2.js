4aa2d63d25b9ce15adb03d094345605225e7ca1d
Ankush Das <ankush.pentester@gmail.com>
2022-12-10 11:51:13 +0530

    Portfolio

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+// import myImg from "../../Assets/a.jpeg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I am Ankush Das, a Computer Science Student.
+              <br />
+              <br />
+              My field of Interest's lies in &nbsp;
+              <i>
+                <b className="purple">Android , Web and Blockchain Development</b> and I
+                love reading <b className="purple">Books.</b>
+              </i>
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple"> Flutter , React Js , Node js and Python </b>
+              </i>
+              <br />
+              <br />
+              When I'm not in front of my screen, I am either found &nbsp;
+              <i>
+                <b className="purple">Reading Any Book </b> or &nbsp;
+                <b className="purple">vibing to any song!</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img
+                style={{ borderRadius: "200px" }}
+                src="https://avatars.githubusercontent.com/u/71139852?v=4"
+                className="img-fluid a"
+                alt="avatar"
+              />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me on
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/anounman"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/anounman"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/anounman/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/anounman/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+
+import tasmeal from "../../Assets/Projects/b.png";
+import ai from "../../Assets/Projects/ai.png";
+import codesync from "../../Assets/Projects/a.png";
+import dict from "../../Assets/Projects/c.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={codesync}
+              isBlog={false}
+              title="Gym Gude Using ML and Flutter"
+              description="ML flutter project it use image processing to detect postion and count your workout
+              "
+              link="https://github.com/anounman/Flutter-ML-Gym-Guide"
+            />
+          </Col>
+
+          <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={tasmeal}
+              isBlog={false}
+              title="ReLation"
+              description="It's a dating app where we carefully make matches. We are here to make our generation best by giving them their best partners. Now we are just making best matches but gradually our app will provide many more advanced advantages that are not given by any other dating apps."
+              link="https://github.com/anounman/ReLatio"
+              website="https://play.google.com/store/apps/details?id=com.in.relation.app"
+            />
+          </Col>
+
+          {/* <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={ai}
+              isBlog={false}
+              title="AI New Reader"
+              description="We have used React Js to design the app. This app is designed for web users. AI New Reader is a platform through which you can search latest news by your voice command. You can also open specific news by giving a command."
+              link="https://github.com/rht-singh/A.I-news.git"
+            />
+          </Col> */}
+
+          <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={dict}
+              isBlog={false}
+              title="Dictonary Website"
+              description="This is a simple whatsapp clone built using react and use firebase a backend. And it use firebase google authenticator for login. You can login with google in it."
+              link="https://github.com/anounman/whatsapp_clone"
+              website="https://reactwpc.web.app/"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
