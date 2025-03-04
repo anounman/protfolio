5865e3a3b8336f68441e9728c6fc4369c38c1f5e
anounman <ankush.pentester@gmail.com>
2025-03-04 19:33:25 +0100

    add three project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
+import { Col, Container, Row } from "react-bootstrap";
 import Particle from "../Particle";
+import ProjectCard from "./ProjectCards";
 
-import tasmeal from "../../Assets/Projects/b.png";
-import ai from "../../Assets/Projects/ai.png";
 import codesync from "../../Assets/Projects/a.png";
+import tasmeal from "../../Assets/Projects/b.png";
 import dict from "../../Assets/Projects/c.png";
+import cvGen from "../../Assets/Projects/cv_gen.png";
+import hirred from "../../Assets/Projects/hirred.png";
+
 
 function Projects() {
   return (
@@ -20,6 +22,36 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={hirred}
+              isBlog={false}
+              title="Hirrd - Job Portal Application"
+              description="Hirrd is a modern job portal application that connects job seekers with employers. The platform enables companies to post job listings and candidates to search and apply for positions that match their skills and interests."
+              link="https://github.com/anounman/Full-Stack-Job-Portal"
+              website="https://full-stack-job-portal.vercel.app/"
+            />
+          </Col>
+          <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={cvGen}
+              isBlog={false}
+              title="ATS-Friendly CV Generator"
+              description="An AI-powered tool that analyzes your existing CV and job descriptions to generate LaTeX code for ATS-optimized resumes. The generated LaTeX code can be compiled into a PDF using Overleaf."
+              link="https://github.com/anounman/ATS-Friendly-Resume-Maker"
+              websit="https://ats-friendly-resume-maker.streamlit.app/"
+            />
+          </Col>
+          <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={cvGen}
+              isBlog={false}
+              title="Responsive Halloween Website"
+              description="It's a responsive Halloween website built using HTML, CSS, and JavaScript to showcase the the htm and css knowladge. It's a simple website with some animations and responsive design."
+              link="https://github.com/anounman/Halloween-Website"
+              websit="https://anounman.github.io/Halloween-Website/"
+            />
+          </Col>
           <Col md={6} className="project-card">
             <ProjectCard
               imgPath={codesync}
@@ -41,17 +73,6 @@ function Projects() {
               website="https://play.google.com/store/apps/details?id=com.in.relation.app"
             />
           </Col>
-
-          {/* <Col md={6} className="project-card">
-            <ProjectCard
-              imgPath={ai}
-              isBlog={false}
-              title="AI New Reader"
-              description="We have used React Js to design the app. This app is designed for web users. AI New Reader is a platform through which you can search latest news by your voice command. You can also open specific news by giving a command."
-              link="https://github.com/rht-singh/A.I-news.git"
-            />
-          </Col> */}
-
           <Col md={6} className="project-card">
             <ProjectCard
               imgPath={dict}
