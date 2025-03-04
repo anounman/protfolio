7deced569648454e0f1e35a63dac4c437a8cb101
anounman <ankush.pentester@gmail.com>
2025-03-04 19:46:52 +0100

    Small chnage on text

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Ankush Das </span>
-            from <span className="purple"> West Bengal, India.</span>
+            from <span className="purple"> Saarland , Germany.</span>
             <br />
             Aside from coding, there are a few other things I enjoy doing!
           </p>
